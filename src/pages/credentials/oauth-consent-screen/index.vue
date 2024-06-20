201572559906b142e2b019d9a8327202b69a66b8
Martien Dermawan Tanama <martiendt@gmail.com>
2024-06-20 10:26:39 +0700

    xcz

--- src/pages/credentials/oauth-consent-screen/index.vue
+++ src/pages/credentials/oauth-consent-screen/index.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { AxiosError } from 'axios'
 import { reactive, ref } from 'vue'
-import { useRouter } from 'vue-router'
 
 import axios from '@/axios'
 import { useToastStore } from '@/stores/toast-store'
@@ -14,21 +13,11 @@ import CardBreadcrumbs from './card-breadcrumbs.vue'
 import { useForm } from './form'
 import type { IAuthorizedDomain } from './types'
 
-const router = useRouter()
 const { toastRef } = useToastStore()
 const form = reactive(useForm())
 
 const authorizedDomains = ref<IAuthorizedDomain[]>([])
 
-const showApiKeyModal = ref(false)
-const toggleApiKeyModal = (value: boolean) => {
-  let newValue = !showApiKeyModal.value
-  if (value === true) newValue = true
-  if (value === false) newValue = false
-  showApiKeyModal.value = newValue
-}
-
-const generatedApiKey = ref()
 const onSave = async () => {
   // convert authorized domains array
   form.data.authorized_domains = authorizedDomains.value.map(function (item) {
@@ -38,9 +27,7 @@ const onSave = async () => {
   try {
     const response = await axios.post('/v1/api-keys', form.data)
     if (response.status === 201) {
-      generatedApiKey.value = response.data.api_key
       toastRef.toast('Create success')
-      toggleApiKeyModal(true)
     }
   } catch (error) {
     if (error instanceof AxiosError) {
@@ -59,17 +46,6 @@ const onSave = async () => {
     }
   }
 }
-
-const onCloseCreatedModal = () => {
-  toggleApiKeyModal(false)
-  router.push('/credentials/api-keys')
-}
-
-const tooltip = ref('copy')
-const onCopyApiKey = () => {
-  navigator.clipboard.writeText(generatedApiKey.value)
-  tooltip.value = 'copied!'
-}
 </script>
 
 <template>
@@ -86,36 +62,12 @@ const onCopyApiKey = () => {
 
     <base-card>
       <div class="flex gap-2">
-        <base-button color="primary" @click="onSave()">Save & Generate Key</base-button>
+        <base-button color="primary" @click="onSave()">Save</base-button>
         <router-link to="/credentials/api-keys">
           <base-button color="danger">Cancel</base-button>
         </router-link>
       </div>
     </base-card>
-
-    <!-- success confirmation, and inform user to save the api key -->
-    <base-modal :is-open="showApiKeyModal" @on-close="onCloseCreatedModal()">
-      <div class="max-h-90vh overflow-auto p-4">
-        <h2 class="py-4 text-2xl font-bold">API Key created</h2>
-        <div class="space-y-8">
-          <p>Make sure to copy your API Key now. You won't be able to see it again!</p>
-          <div class="flex flex-col gap-2">
-            <span class="font-semibold">Your API Key</span>
-            <div class="w-full border border-black flex items-center gap-2 py-2">
-              <base-button size="sm" v-tooltip="tooltip" @click="onCopyApiKey">
-                <base-icon icon="i-far-copy"></base-icon>
-              </base-button>
-              <div>{{ generatedApiKey }}</div>
-            </div>
-          </div>
-          <div class="flex gap-2">
-            <base-button color="primary" size="sm" @click="toggleApiKeyModal(false)">
-              Close
-            </base-button>
-          </div>
-        </div>
-      </div>
-    </base-modal>
   </div>
 </template>
 
